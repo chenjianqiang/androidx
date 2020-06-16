6cc32c25a644061e7bb58901ad5e4d46c59d1d3d
cjq <chenjian_123qiang@163.com>
2020-06-16 08:48:17 +0800

    增加blankj工具

--- app/src/main/java/com/cjq/androidx/activity/AopDemoActivity.java
+++ app/src/main/java/com/cjq/androidx/activity/AopDemoActivity.java
@@ -1,21 +1,35 @@
 package com.cjq.androidx.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.cjq.androidx.R;
 import com.cjq.androidx.databinding.ActivityAopDemoBinding;
 
 
-public class AopDemoActivity extends AppCompatActivity {
+public class AopDemoActivity extends AppCompatActivity implements View.OnClickListener {
     private ActivityAopDemoBinding mView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mView = DataBindingUtil.setContentView(this, R.layout.activity_aop_demo);
+        mView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_as:
+                ToastUtils.showShort("你点击的是某个按钮");
+                break;
+        }
 
     }
 }
